24f4efd1b2777735d56e6781ccbd77cede229418
Moutoussamy <57101711+Moutoussamy@users.noreply.github.com>
2020-05-18 09:45:47 +0200

    entete

---
+++ aMD_parameters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Propose parmaters for accelerated molecular dynamics based of a classical molecular dynamics
+"""
+
+__author__ = "Emmanuel Edouard MOUTOUSSAMY"
+__version__  = "1.0.0"
+__copyright__ = "copyleft"
+__date__ = "2020/05"
+
+import sys
+import os
+import numpy as np
+
+
+
